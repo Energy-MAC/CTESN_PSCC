510be8d7d30a9930b1b3a26276dadf002a16207b
Ciaran Roberts <ciaran@gigteam.lbl.gov>
2022-02-06 15:53:01 -0800

    Adding non-linear projection

--- src/ctesn_functions.jl
+++ src/ctesn_functions.jl
@@ -83,3 +83,60 @@ function linear_predict(p, f, rSol, simStep, N, resSize)
     pred = Woutpred * reduce(hcat, rSol.(simStep))
     pred
 end
+
+function nonlinear_mapping(sys, busCap, lb, ub, trainSize, totalPower, rSol, stateIndex, simStep)
+    
+    # The two parametes are 1) the % of IBR at each node and 2) the % of those IBR that are grid-forming
+    boundarySamples=reduce(hcat, vec([[x;y] for x in [lb[1], ub[1]], y in [lb[2], ub[2]]]))
+    interiorSamples = QuasiMonteCarlo.sample(trainSize-size(boundarySamples)[2], lb, ub, QuasiMonteCarlo.SobolSample()) # Sample parameter sapce
+    trainSamples = [boundarySamples interiorSamples]
+    
+    gen = PSY.get_component(PSY.DynamicGenerator, sys, "generator-2-Trip")
+    genTrip = GeneratorTrip(10.0, gen)
+    tspan=(0.0, 70.0)
+    
+    function get_W_rbf(p)
+        Gf=p[1]*(1-p[2]) # Grid following %
+        GF=p[1]*p[2] # Grid forming %
+        
+        sys=change_ibr_penetration(sys, GF, Gf, ibrBus, busCap, totalPower) # Change generation mix at each of the IBR nodes
+        sim = Simulation!(
+            ResidualModel, #Type of model used
+            sys,         #system
+            file_dir,       #path for the simulation output
+            tspan, #time span
+            genTrip,
+        )
+
+        res = small_signal_analysis(sim)
+
+        execute!(sim, IDA())
+        
+        R = reduce(hcat, rSol.(simStep))
+        S = reduce(hcat, (sim.results.solution.(simStep)))[stateIndex, :]
+       
+        UB=vec(maximum(R, dims=2));
+        LB=vec(minimum(R, dims=2));
+
+        Input = [R[:,i] for i in 1:size(R,2)];
+        Output = [S[:,i] for i in 1:size(S,2)];
+        
+        surr = RadialBasis(Input, Output, LB, UB, rad = linearRadial)
+        beta = surr.coeff
+        Wout = (svd(R') \ S')' # calculate read-out matrix
+        beta, surr, R, S
+    end
+
+    res = pmap(x -> get_W_rbf(trainSamples[:,x]), 1:trainSize) # Distribute computaiton among all available workers
+    Woutparams = [trainSamples[:,i] for i = 1:size(trainSamples, 2)] # Distribute computaiton among all available workers
+    Wouts = map(x -> x[1], res) # get read-out matrices from returned res object
+    surr=res[1][2]
+    Woutparams, Wouts, surr
+end
+
+function nonlinear_predict(p, betaSurr, Surr, rSol, simStep, N, resSize)
+    weights=reshape(betaSurr(p), size(Surr.coeff)[1], size(Surr.coeff)[2])
+    Surr.coeff = weights
+    pred = reduce(hcat, Surr.(rSol(simStep).u))
+    pred
+end
---
+++ trainSizeNL.jl
@@ -0,0 +1,96 @@
+using Pkg
+Pkg.activate(".")
+using PowerSimulationsDynamics
+PSID = PowerSimulationsDynamics
+using PowerSystems
+using Logging
+using Sundials
+using LightGraphs
+using Plots
+using OrdinaryDiffEq
+using QuasiMonteCarlo
+using LinearAlgebra
+using Surrogates
+using CSV
+using DataFrames
+using Statistics
+using Distributed
+using Random
+using Plots.PlotMeasures
+using Interpolations
+using StatsPlots # no need for `using Plots` as that is reexported here
+Random.seed!(1234)
+
+file_dir = joinpath(pwd(), "src",)
+include(joinpath(file_dir, "models/system_models.jl"))
+include(joinpath(file_dir, "ctesn_functions.jl"))
+include(joinpath(file_dir, "experimentParameters.jl"))
+
+sys, busCap, totalPower = buid_system(ibrBus, GF, Gf);   # Build the system
+gen = PSY.get_component(PSY.DynamicGenerator, sys, "generator-2-Trip")
+genTrip = GeneratorTrip(tripTime, gen)
+
+rSol, N, stateIndex, simStep = simulate_reservoir(sys, resSize, maxSimStep);
+
+testParams = QuasiMonteCarlo.sample(testSize, LB, UB, QuasiMonteCarlo.SobolSample()) # Sample parameter sapce
+
+freq_error=zeros(testSize, length(trainSizes))
+rocof_error=zeros(testSize,length(trainSizes))
+nadir_error=zeros(testSize,length(trainSizes))
+
+for i in 1:length(trainSizes)
+    
+    trainParams, Wouts, surr = linear_mapping(sys, busCap, LB, UB, trainSizes[i], totalPower, rSol, stateIndex, simStep);
+    betaSurr = RadialBasis(trainParams, Wouts, LB, UB, rad = linearRadial)
+    
+    for j in 1:testSize
+        Gf=testParams[1,j]*(1-testParams[2,j]) # Grid following %
+        GF=testParams[1,j]*testParams[2,j] # Grid forming %
+        
+        sys=change_ibr_penetration(sys, GF, Gf, ibrBus, busCap, totalPower) # Change generation mix at each of the IBR nodes
+        sim = Simulation!(
+            ResidualModel, #Type of model used
+            sys,         #system
+            file_dir,       #path for the simulation output
+            tspan, #time span
+            genTrip,
+        )
+
+        res = small_signal_analysis(sim);
+
+        execute!(sim, IDA())
+   
+        sol_array = Array(sim.results.solution(interpolateTime)) # Convert physcial solution to an array
+        pred=nonlinear_predict([testParams[1,j], testParams[2,j], betaSurr, surr, rSol, simStep, N, resSize)
+        resamplePred=transpose(reduce(hcat, [LinearInterpolation(simStep, pred[i, :]).(interpolateTime) for i in 1:size(pred)[1]]))
+                
+        nadir_error[j,i]=minimum(sol_array[stateIndex, :]) - minimum(pred) # Find difference between lowest predicted frequency and actual lowest frequency
+        freq_error[j,i]=norm(sol_array[stateIndex, :] - resamplePred, Inf) # Find the largest error
+        rocof_error[j,i]=(minimum(diff(sol_array[stateIndex, :], dims=2))-minimum(diff(resamplePred, dims=2)))/interpolateStep # Find the largest error of numerically 
+    end
+end
+
+nadir_plot=boxplot(freq_base*nadir_error[:,1], label="", xlabel="Training Size", ylabel="Nadir prediction error [Hz]")
+for i in 2:length(trainSizes)
+    boxplot!(freq_base*nadir_error[:, i], label="")
+end
+xticks!([1:1:length(trainSizes);], string.(trainSizes))
+savefig("results/figs/nonLinearNadirError.pdf")
+
+max_error_plot=boxplot(freq_base*freq_error[:,1], label="", xlabel="Training Size", ylabel="Maximum prediction error [Hz]")
+for i in 2:length(trainSizes)
+    boxplot!(freq_base*freq_error[:,i], label="")
+end
+xticks!([1:1:length(trainSizes);], string.(trainSizes))
+savefig("results/figs/nonLinearMaxError.pdf")
+
+rocof_plot=boxplot(freq_base*rocof_error[:,1], label="", xlabel="Training Size", ylabel="Maximum RoCoF error [Hz/s]")
+for i in 2:length(trainSizes)
+    boxplot!(freq_base*rocof_error[:,i], label="")
+end
+xticks!([1:1:length(trainSizes);], string.(trainSizes))
+savefig("results/figs/nonLinearRocofError.pdf")
+
+CSV.write("results/data/nonLinearFreqErrors.csv", DataFrame(freq_error, :auto), header = string.(trainSizes))
+CSV.write("results/data/nonLinearRocofErrors.csv", DataFrame(rocof_error, :auto), header = string.(trainSizes))
+CSV.write("results/data/nonLinearNadirErrors.csv", DataFrame(nadir_error, :auto), header = string.(trainSizes))
